7ae32b94fe9c3c6a7f6189f9a9188ef27f6c03d2
Yeison Valencia <yvalencia@tech-synergy.co>
2024-04-06 13:25:23 -0500

    feat(client) se agregan enlaces en los botones

--- scripts.js
+++ scripts.js
@@ -1,31 +1,34 @@
-window.onload = function() {
-  const whatsAppButton = document.querySelector('.whatsapp-button');
-  whatsAppButton.addEventListener('click', function() {
-    const whatsAppLink = "https://wa.me/qr/RBXWHSNMZZKYI1";
+window.onload = function () {
+  const whatsAppButton = document.querySelector(".whatsapp-button");
+  whatsAppButton.addEventListener("click", function () {
+    const whatsAppLink =
+      "https://wa.me/573042830114?text=Quiero%20informaci%C3%B3n%20sobre%20lo%20que%20ofrece%20Tech-Synergy%20Innovations%20S.A.S.";
     window.location.href = whatsAppLink;
   });
 
-  const webButton = document.querySelector('.web-button');
-  webButton.addEventListener('click', function() {
+  const webButton = document.querySelector(".web-button");
+  webButton.addEventListener("click", function () {
     const webLink = "https://www.tech-synergy.co/";
     window.location.href = webLink;
   });
 
-  const linkedinButton = document.querySelector('.linkedin-button');
-  linkedinButton.addEventListener('click', function() {
-    const linkedinLink = "https://www.linkedin.com/company/101111091/admin/feed/posts/";
+  const linkedinButton = document.querySelector(".linkedin-button");
+  linkedinButton.addEventListener("click", function () {
+    const linkedinLink =
+      "https://www.linkedin.com/company/101111091/admin/feed/posts/";
     window.location.href = linkedinLink;
   });
 
-  const instagramButton = document.querySelector('.instagram-button');
-  instagramButton.addEventListener('click', function() {
+  const instagramButton = document.querySelector(".instagram-button");
+  instagramButton.addEventListener("click", function () {
     const instagramLink = "https://www.instagram.com/techsynergyinnovations/";
     window.location.href = instagramLink;
   });
 
-  const facebookButton = document.querySelector('.facebook-button');
-  facebookButton.addEventListener('click', function() {
-    const facebookLink = "https://www.facebook.com/profile.php?id=61556304670570";
+  const facebookButton = document.querySelector(".facebook-button");
+  facebookButton.addEventListener("click", function () {
+    const facebookLink =
+      "https://www.facebook.com/profile.php?id=61556304670570";
     window.location.href = facebookLink;
   });
 };
